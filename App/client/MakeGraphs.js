21cf5544a060ffa65e45b662f1a87459cdb1daf1
jonathan <jonan099@student.liu.se>
2016-12-16 17:17:53 +0100

    Added local versions of javascripts for offline work. Plotted on id instead of type. Removed links to previous versions and to other start nodes. Made graphs size gigantic to be able to see the whole graph.

--- App/client/MakeGraphs.js
+++ App/client/MakeGraphs.js
@@ -5,14 +5,6 @@ var dagD3Draw = require('dagre-d3'); // Library for drawing graph on canvas
 class MakeGraphs {
     constructor(Graphs) {
         this.graphs = Graphs;
-        this.acceptedEventTypes = [
-            'BASE',
-            'CHANGE',
-            'ELEMENT',
-            "COMPOSITION",
-            "CAUSE",
-            "CONTEXT"
-        ];
     }
     makeGraphRecursive(startNode, g, preventCycles){
         if (preventCycles.indexOf(startNode.meta.id) > -1){
@@ -20,16 +12,25 @@ class MakeGraphs {
         }
         let decorate = Decorator.decorateNode(startNode);
         g.setNode(
-            startNode.meta.type,
+            startNode.meta.id,
             {label: decorate[0], style: decorate[1], shape: decorate[2]}
         );
         preventCycles.push(startNode.meta.id);
         for (let j = 0; j < startNode.nextActivities.length; j++) {
-            //if( this.acceptedEventTypes.indexOf(startNode.nextActivities[j].type) > -1){  // Only use accepted types.
-                g.setEdge(startNode.meta.type, startNode.nextActivities[j].ref.meta.type, {});
+            if(startNode.nextActivities[j].type == "PREVIOUS_VERSION"){  //Skip all links that point back to earlier versions.
+                continue;
+            }
+            if(startNode.nextActivities[j].ref.meta.type == "EiffelSourceChangeSubmittedEvent"){  //Skip all source changes except the first.
+                let tmp = preventCycles.length == 1;
+                if(!tmp){
+                    continue;
+                }
+
+            }
+                g.setEdge(startNode.meta.id, startNode.nextActivities[j].ref.meta.id, {});
 
                 this.makeGraphRecursive(startNode.nextActivities[j].ref, g, preventCycles);
-            //}
+
         }
     }
     makeGraph(gd) {
